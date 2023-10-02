5cac6067ca66dca9496863b09a1e09e8f5f32699
erji ridho lubis <09011281924072@student.unsri.ac.id>
2023-10-02 22:58:32 +0700

    add model product

--- internal/model/product_model.go
+++ internal/model/product_model.go
@@ -1,1 +1,26 @@
 package model
+
+type Product struct {
+	ID          string  `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       uint64  `json:"price"`
+	Stock       uint64  `json:"stock"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+}
+
+type ProductRequest struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       uint64  `json:"price"`
+	Stock       uint64  `json:"stock"`
+}
+
+type ProductResponse struct {
+	ID          string  `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       uint64  `json:"price"`
+	Stock       uint64  `json:"stock"`
+}
