164fa85e3620355d4d853f37ec64c94ac47ea00a
erji ridho lubis <09011281924072@student.unsri.ac.id>
2023-10-03 15:37:11 +0700

    add encrypt and compare password

--- common/common.go
+++ common/common.go
@@ -1,6 +1,11 @@
 package common
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 var (
 	KindCollection = "collection"
@@ -8,12 +13,35 @@ var (
 	KindProduct    = "product"
 	KindCartItem   = "cart-item"
 	KindCategory   = "category"
+	KindAuthorization = "authorization"
 
 	MessageCreateSuccess    = "Successfully adding data"
 	ValidationFailedMessage = "Validation Failed"
+	
 	ErrNotFound             = "Data not found"
+	ErrInvalidPassword		= "Invalid password"
 )
 
 func GenerateUUID() string {
 	return uuid.New().String()
+}
+
+func HashAndSalt(pwd []byte) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	return string(hash), err
+}
+
+func ComparePasswords(hashedPwd string, plainPwd []byte) bool {
+    byteHash := []byte(hashedPwd)
+    err := bcrypt.CompareHashAndPassword(byteHash, plainPwd)
+    if err != nil {
+        fmt.Println(err)
+        return false
+    }
+    
+    return true
 }
