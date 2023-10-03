7dc366e5a9ce0c74f5e1fc4d90b4f8f7fe7e0054
erji ridho lubis <09011281924072@student.unsri.ac.id>
2023-10-03 11:04:25 +0700

    add validation response

--- common/response_rest.go
+++ common/response_rest.go
@@ -12,6 +12,12 @@ type ApiErrorResponseModel struct {
 	Error ErrorResponseModel `json:"error"`
 }
 
+type ErrorValidationResponseModel struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Errors  interface{} `json:"errors"`
+}
+
 func SuccessResponse(c *fiber.Ctx, status int, data interface{}) error {
 	return c.Status(status).JSON(data)
 }
@@ -22,5 +28,13 @@ func ErrorResponseRest(c *fiber.Ctx, code int, errStr string) error {
 			Code:    code,
 			Message: errStr,
 		},
+	})
+}
+
+func ErrorValidationResponse(c *fiber.Ctx, code int, errStr string, err interface{}) error {
+	return c.Status(code).JSON(ErrorValidationResponseModel{
+		Code:    code,
+		Message: errStr,
+		Errors:  err,
 	})
 }
