e0387a3b4f3ffaa9cc574312e8d0367d8a123961
erji ridho lubis <09011281924072@student.unsri.ac.id>
2023-10-03 11:04:13 +0700

    add generate uuid

--- common/common.go
+++ common/common.go
@@ -1,13 +1,19 @@
 package common
 
-var (
-	KindCollection   = "collection"
-	KindUser         = "user"
-	KindProduct      = "product"
-	KindCartItem     = "cart-item"
-	KindCategory     = "category"
+import "github.com/google/uuid"
 
-	MessageCreateSuccess	= "Successfully adding data"
-	ValidationFailedMessage	= "Validation Failed"	
-	ErrNotFound				= "Data not found"
+var (
+	KindCollection = "collection"
+	KindUser       = "user"
+	KindProduct    = "product"
+	KindCartItem   = "cart-item"
+	KindCategory   = "category"
+
+	MessageCreateSuccess    = "Successfully adding data"
+	ValidationFailedMessage = "Validation Failed"
+	ErrNotFound             = "Data not found"
 )
+
+func GenerateUUID() string {
+	return uuid.New().String()
+}
